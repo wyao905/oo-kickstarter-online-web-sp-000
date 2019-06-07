cf13541936a43d7a82f2c16960b2e27c5defdf28
Warren Yao <warrenyca905@gmail.com>
2019-06-07 01:46:02 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
 class Backer
-  attr_accessor :name, :backed_projects
-  # attr_reader :backed_projects
+  attr_accessor :name
+  attr_reader :backed_projects
   
   def initialize(name)
     @backed_projects = []
